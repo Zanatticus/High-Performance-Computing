3ecc9a60aebc7c677c0356031d2598bd6fe93468
Zanatticus <zingare@live.com>
2025-02-17 01:12:56 -0500

    initial prob 2 structure

---
+++ Homework/HW2/Problem2/src/philosophers_forks.cpp
@@ -0,0 +1,11 @@
+// Program for finding the solution to the dining philosophers problem using pthreads
+// The solution should be for an unbounded odd number of philosophers, with each philosopher implemented as a thread, and the forks are the synchronizations needed between them.
+// Author: Zander Ingare
+
+#include <iostream>
+#include <pthread.h>
+
+int main() {
+    std::cout << "Philosophers with forks!" << std::endl;
+    return 0;
+}
