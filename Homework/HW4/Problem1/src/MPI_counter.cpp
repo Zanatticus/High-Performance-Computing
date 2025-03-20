a8510837aef4cbfe7e88846f0ffd7a3e0e589b93
Zanatticus <zingare@live.com>
2025-03-20 15:19:10 -0400

    p1

--- Homework/HW4/Problem1/src/MPI_counter.cpp
+++ Homework/HW4/Problem1/src/MPI_counter.cpp
@@ -5,45 +5,38 @@
 #include <mpi.h>
 
 int main(int argc, char** argv) {
-    int world_rank, world_size;
-    const int COUNT_LIMIT = 64;
+    int rank, size;
+    const int COUNT_LIMIT = 48;
     int counter = 0;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int processor_name_len;
     MPI_Status status;
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Get_processor_name(processor_name, &processor_name_len);
 
-
-    
-    // Print total number of ranks from process 0
-    if (world_rank == 0) {
-        std::cout << "Total MPI ranks allocated: " << world_size << std::endl;
-    }
-    
-    // Process 0 starts the counter
-    if (world_rank == 0) {
-        counter = 0;  // Start with 1
-        std::cout << "Process " << world_rank << " has counter value " << counter << std::endl;
-        counter++;  // Increment before sending
-        MPI_Send(&counter, 1, MPI_INT, world_rank + 1, 0, MPI_COMM_WORLD);
+    if (size < COUNT_LIMIT) {
+        if (rank == 0) {
+            std::cout << "Error: Expected " << COUNT_LIMIT << " MPI ranks, but got " << size << std::endl;
+        }
+        MPI_Finalize();
+        return 1;
     }
 
-    while (counter < COUNT_LIMIT) {
-        // All other processes receive from previous rank
-        if (world_rank != 0) {
-            MPI_Recv(&counter, 1, MPI_INT, (world_rank - 1 + world_size) % world_size, 0, MPI_COMM_WORLD, &status);
-            std::cout << "Process " << world_rank << " has counter value " << counter << std::endl;
-            
-            // Only increment and send if we haven't reached the limit
-            if (counter < COUNT_LIMIT) {
-                counter++;
-                // Send to next process (wrap around to 0 if we're the last process)
-                MPI_Send(&counter, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
-            }
+    if (rank == 0) {
+        counter = 1;
+        std::cout << "Process " << rank << " on node " << processor_name << " received value: " << counter << "\n";
+        counter++;
+        MPI_Send(&counter, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+    } else {
+        MPI_Recv(&counter, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &status);
+        std::cout << "Process " << rank << " on node " << processor_name << " received value: " << counter << "\n";
+
+        if (rank < COUNT_LIMIT - 1) {
+            counter++;
+            MPI_Send(&counter, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
         }
     }
 
