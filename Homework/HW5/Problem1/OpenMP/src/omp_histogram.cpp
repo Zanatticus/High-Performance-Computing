fb6f10ae2f7aa34fa9d80a5d1b964a0c708a3b5c
Zanatticus <zingare@live.com>
2025-03-29 21:46:09 -0400

    cleaner omp

--- Homework/HW5/Problem1/OpenMP/src/omp_histogram.cpp
+++ Homework/HW5/Problem1/OpenMP/src/omp_histogram.cpp
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <iostream>
 #include <omp.h>
+#include <chrono>
 
 #define N        (1 << 23)
 #define RANGE    100000
@@ -12,25 +13,19 @@
 #define NUM_THREADS 1
 
 int main() {
-	// Set the number of threads to use
 	omp_set_num_threads(NUM_THREADS);
 
-    // Random data set ranging from 1 to RANGE, inclusive
     int* data = new int[N];
     for (int i = 0; i < N; i++) {
         data[i] = rand() % RANGE + 1;
     }
 
-    // Calculate bin width
-    double bin_width = static_cast<double>(RANGE) / NUM_BINS;
+    float bin_width = static_cast<float>(RANGE) / NUM_BINS;
 
-    // Get number of threads and thread ID info
-    double start_time = omp_get_wtime();
-
-    // Allocate histogram (shared)
     int global_histogram[NUM_BINS] = {0};
-
 	int local_histograms[NUM_THREADS][NUM_BINS] = {0};
+
+	auto start = std::chrono::high_resolution_clock::now();
 
 	#pragma omp parallel
 	{
@@ -38,22 +33,23 @@ int main() {
 
 		#pragma omp for
 		for (int i = 0; i < N; i++) {
-			int bin = floor((data[i] - 1) / bin_width);
+			int bin = floorf((data[i] - 1) / bin_width);
 			if (bin >= NUM_BINS) bin = NUM_BINS - 1;
 			local_histograms[tid][bin]++;
 		}
 	}
 
+	// Combine local histograms into global histogram
 	for (int t = 0; t < NUM_THREADS; ++t) {
+		// #pragma omp parallel for
 		for (int b = 0; b < NUM_BINS; ++b) {
 			global_histogram[b] += local_histograms[t][b];
 		}
 	}
 
-    double end_time = omp_get_wtime();
-    double elapsed_time = end_time - start_time;
+	auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
 
-    // Print results
     std::cout << "OpenMP Histogram Results\n";
     std::cout << "============================\n\n";
     std::cout << "Total elements: " << N << " | Range: 1-" << RANGE
@@ -61,10 +57,10 @@ int main() {
     std::cout << "OpenMP Threads: " << NUM_THREADS << "\n\n";
     std::cout << "Performance Metrics:\n";
     std::cout << "  └── Total Time: " << std::fixed << std::setprecision(10)
-              << elapsed_time << " seconds\n\n";
+              << elapsed.count() << " seconds\n\n";
 
     // for (int i = 0; i < NUM_BINS; i++) {
-    //     int bin_start = (i == 0) ? 1 : floor(i * bin_width) + 1;
+    //     int bin_start = (i == 0) ? 1 : floorf(i * bin_width) + 1;
     //     int bin_end = floor((i + 1) * bin_width);
     //     if (i == NUM_BINS - 1) bin_end = RANGE;
 
